348dc0d3cadd0ffac1716f8eda0160a3facd3be3
Mitchell Ciupak <42656862+mitchellciupak@users.noreply.github.com>
2020-05-19 17:29:07 -0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -63,7 +63,7 @@ class Dqn():
         self.last_reward = 0
     
     def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # Temp = 100 (Range 0 to 100)
         action = probs.multinomial(1)
         return action.data[0,0]
     
